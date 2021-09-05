995e9d5051d459fad10607898ce5bfc41731ef29
c64ren <nicolaeflorin@protonmail.com>
2021-09-04 22:32:28 -0400

    Met à jour le contenu du cours 8

--- cours/Cours08/content.php
+++ cours/Cours08/content.php
@@ -1,6 +1,116 @@
-<section id="" class="">
-    <h1></h1>
-    <p>
+<section id="Switch" class="contenuChapitre">
+    <h1>Switch</h1>
+    <p>On peut Utiliser l'instruction switch pour sélectionner l'un des nombreux blocs de code à exécuter.</p>
+    <p>Lorsque JavaScript atteint un mot-clé break, il sort du bloc switch.</p>
+    <p>Cela arrêtera l'exécution à l'intérieur du switch bloc.</p>
+    <p class="codepen" data-height="299" data-theme-id="dark" data-default-tab="js" data-slug-hash="bGRwaOQ" data-user="c64ren" style="height: 299px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/bGRwaOQ">
+                switch</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="Boucle For" class="contenuChapitre">
+    <h1>La boucle <code>For</code></h1>
+    <p class="codepen" data-height="223" data-theme-id="dark" data-default-tab="js" data-slug-hash="XWgjVOm" data-user="c64ren" style="height: 223px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/XWgjVOm">
+                boucleFor</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <p>On peut initialiser de nombreuses variables dans l'instruction 1 (séparées par une virgule).</p>
+    <p class="codepen" data-height="279" data-theme-id="dark" data-default-tab="js" data-slug-hash="VwWKygz" data-user="c64ren" style="height: 279px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/VwWKygz">
+                boucleFor2</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <p>On peut ignorer l'instruction 1.</p>
+    <p class="codepen" data-height="301" data-theme-id="dark" data-default-tab="js" data-slug-hash="OJgRzdw" data-user="c64ren" style="height: 301px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/OJgRzdw">
+                boucleFor3</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="For In" class="contenuChapitre">
+    <h1>For In</h1>
+    <p>L'instruction JavaScript <code>for</code> / <code>in</code> parcourt les propriétés d'un objet</p>
+    <p class="codepen" data-height="334" data-theme-id="dark" data-default-tab="js" data-slug-hash="MWojrxW" data-user="c64ren" style="height: 334px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/MWojrxW">
+                forIn</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <p class="codepen" data-height="263" data-theme-id="dark" data-default-tab="js" data-slug-hash="VwWKyRM" data-user="c64ren" style="height: 263px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/VwWKyRM">
+                forIn2</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="For Of" class="contenuChapitre">
+    <h1>For Of</h1>
+    <p>L'instruction JavaScript for/of parcourt les valeurs d'un objet itérable. </p>
+    <p>Pour chaque itération, la valeur de la propriété suivante est affectée à x. La variable peut être déclarée avec const, let ou var.</p>
+    <p class="codepen" data-height="243" data-theme-id="dark" data-default-tab="js" data-slug-hash="bGRwaZO" data-user="c64ren" style="height: 243px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/bGRwaZO">
+                forOf</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <p>For Of permet de boucler sur des structures de données itérables telles que des tableaux, des chaînes, etc.</p>
+    <p class="codepen" data-height="224" data-theme-id="dark" data-default-tab="js" data-slug-hash="JjJRMVE" data-user="c64ren" style="height: 224px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/JjJRMVE">
+                forOfString</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="While" class="contenuChapitre">
+    <h1>La Boucle While</h1>
+    <p>La boucle while parcourt un bloc de code tant qu'une condition spécifiée est vraie.</p>
+    <p class="codepen" data-height="242" data-theme-id="dark" data-default-tab="js" data-slug-hash="powEpBM" data-user="c64ren" style="height: 242px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/powEpBM">
+                while</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="Do While" class="contenuChapitre">
+    <h1>Do While</h1>
+    <p>La boucle do/while est une variante de la boucle while.</p>
+    <p>Cette boucle exécutera le bloc de code une fois, avant de vérifier si la condition est vraie, puis elle répétera la boucle tant que la condition est vraie.</p>
+    <p class="codepen" data-height="262" data-theme-id="dark" data-default-tab="js" data-slug-hash="qBjapGr" data-user="c64ren" style="height: 262px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/qBjapGr">
+                doWhile</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="Break" class="contenuChapitre">
+    <h1>Break</h1>
+    <p>L'instruction <code>break</code> saute une boucle.</p>
+    <p class="codepen" data-height="251" data-theme-id="dark" data-default-tab="js" data-slug-hash="QWgKaRP" data-user="c64ren" style="height: 251px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/QWgKaRP">
+                break</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="Continue" class="contenuChapitre">
+    <h1>Continue</h1>
+    <p>L'instruction <code>continue</code> interrompt une itération (dans la boucle), si une condition spécifiée se produit, et continue avec l'itération suivante dans la boucle.</p>
+    <p class="codepen" data-height="243" data-theme-id="dark" data-default-tab="js" data-slug-hash="zYzKpVB" data-user="c64ren" style="height: 243px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/zYzKpVB">
+                continue</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+</section>
+
+<section id="Label" class="contenuChapitre">
+    <h1>Label</h1>
+    <p>Avec une référence d'étiquette, l'instruction break peut être utilisée pour sortir de n'importe quel bloc de code.</p>
+    <p class="codepen" data-height="391" data-theme-id="dark" data-default-tab="js" data-slug-hash="mdwrpZL" data-user="c64ren" style="height: 391px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/c64ren/pen/mdwrpZL">
+                label</a> by c64ren (<a href="https://codepen.io/c64ren">@c64ren</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
     </p>
 </section>
 
